ef248e927da70b44f93627ef943f08622703c247
Cristian Tala <yomismo@cristiantala.cl>
2020-03-21 13:53:20 -0300

    initial release

---
+++ index.js
@@ -0,0 +1,33 @@
+/**
+ * Clase que se encarga de tener los requerimientos para firmar el mensaje
+ */
+const crypto = require('crypto');
+
+export default class Signature {
+    constructor() {
+
+    }
+
+    static signPayload(payload, secret, prefix = "x_", signature = "signature") {
+        //El arreglo SIEMPRE debe de estar ordenado antes de firmar.
+        let sortedArray = Object.entries(payload).sort();
+        console.log("ARREGLO: ", payload);
+        let payloadFirmado = "";
+        let firma = prefix + signature;
+        let mensaje = "";
+        for (let index = 0; index < sortedArray.length; index++) {
+            console.log(sortedArray[index]);
+            if (sortedArray[index][0] != firma && typeof sortedArray[index][1] !== 'object') {
+                mensaje += sortedArray[index][0] + sortedArray[index][1];
+            }
+        }
+        let hmac = crypto.createHmac('sha256', secret);
+        hmac.setEncoding('hex');
+        hmac.write(mensaje);
+        hmac.end();
+        payloadFirmado = hmac.read();
+        return payloadFirmado;
+    }
+
+
+};
